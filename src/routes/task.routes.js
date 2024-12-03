a48456e171337b4bf00b97ee2a02de961feae642
frank <franklinevillanueva4@gmail.com>
2024-12-02 20:38:33 -0600

    first commit

---
+++ src/routes/task.routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import { authRequired } from '../middlewares/validateToken.js';
+import {
+	getTasks,
+	getTask,
+	createTask,
+	updateTask,
+	deleteTask,
+} from '../controllers/task.controller.js';
+
+const router = Router();
+
+router.get('/tasks', authRequired, getTasks);
+router.get('/tasks/:id', authRequired, getTask);
+router.post('/tasks', authRequired, createTask);
+router.delete('/tasks/:id', authRequired, updateTask);
+router.put('/tasks/:id', authRequired, deleteTask);
+
+export default router;
